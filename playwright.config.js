73ce68cae7f7ef0e9c60ac3b6c4a5592d6f418d0
Petri Vierimaa <pvierimaa@gmail.com>
2024-11-27 10:32:15 +0200

    Added deployment pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:3001',
-    timeout: 120 * 1000,
+    url: 'http://localhost:3001',
+    timeout: 180 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: 'http://localhost:3001/',
+    baseURL: 'http://localhost:3001',
   },
 })

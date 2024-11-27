ea8f9b90cecff2848e3106e4bdcd496ea0c678d3
Petri Vierimaa <pvierimaa@gmail.com>
2024-11-27 11:13:18 +0200

    Moved server startup to the pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3001',
-    timeout: 180 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
   use: {
     baseURL: 'http://localhost:3001',
   },
